98013d9c0c4ffaf90b34e9168f62edd931dc1b10
rouce <lukasbopp@hotmail.com>
2016-06-26 19:01:48 +0200

    fixed sms verification

--- server/sms.js
+++ server/sms.js
@@ -1,14 +1,24 @@
 import { Meteor } from "meteor/meteor";
 import { Accounts } from "meteor/accounts-base";
-import { SMS } from "meteor/accounts-base";
+import { SMS } from "meteor/okland:accounts-phone";
 
-if(Meteor.settings && Meteor.settings.ACCOUNTS_PHONE){
-  Accounts._options.adminPhoneNumbers = Meteor.settings.ACCOUNTS_PHONE.ADMIN_NUMBERS;
-  Accounts._options.phoneVerificationMasterCode = Meteor.settings.ACCOUNTS_PHONE.MASTER_CODE;
+if(Meteor.settings){
+  if(Meteor.settings.ACCOUNTS_PHONE){
+    Accounts._options.adminPhoneNumbers = Meteor.settings.ACCOUNTS_PHONE.ADMIN_NUMBERS;
+    Accounts._options.phoneVerificationMasterCode = Meteor.settings.ACCOUNTS_PHONE.MASTER_CODE;
+  }
 
-  SMS.twilio = {
-    FROM: Meteor.settings.twilio.FROM,
-    ACCOUNT_SID: Meteor.settings.twilio.ACCOUNT_SID,
-    AUTH_TOKEN: Meteor.settings.twilio.AUTH_TOKEN,
+  if(Meteor.settings.TWILIO){
+    SMS.twilio = {
+      FROM: Meteor.settings.TWILIO.FROM,
+      ACCOUNT_SID: Meteor.settings.TWILIO.ACCOUNT_SID,
+      AUTH_TOKEN: Meteor.settings.TWILIO.AUTH_TOKEN,
+    }
+    SMS.phoneTemplates = {
+      from: Meteor.settings.TWILIO.FROM,
+      text: (user, code) => {
+        return "Your verification code: " + code;
+      }
+    };
   }
 }
